775fb6394bd6ef3e49855d49cecdabb3d481c2de
grandeljay <github@grandels.email>
2024-04-11 09:20:16 +0200

    feat(quote): add volumetric weight support

--- src-mmlc/Pallet.php
+++ src-mmlc/Pallet.php
@@ -12,17 +12,36 @@ namespace Grandeljay\Freight;
 
 class Pallet
 {
-    private float $weight   = 0;
     private array $products = [];
 
     public function getWeight(): float
     {
-        return $this->weight;
+        $pallet_weight = 0;
+
+        foreach ($this->products as $product) {
+            $products_length = $product['length'] ?? 0;
+            $products_width  = $product['width']  ?? 0;
+            $products_height = $product['height'] ?? 0;
+            $products_weight = $product['weight'] ?? 0;
+
+            if ($products_length > 0 && $products_width > 0 && $products_height > 0) {
+                $volumetric_weight = ($products_length * $products_width * $products_height) / 5000;
+
+                if ($volumetric_weight > $products_weight) {
+                    $pallet_weight += $volumetric_weight;
+                } else {
+                    $pallet_weight += $products_weight;
+                }
+            } else {
+                $pallet_weight += $products_weight;
+            }
+        }
+
+        return $pallet_weight;
     }
 
     public function addProduct(array $product): void
     {
-        $this->products[] = $product['id'];
-        $this->weight    += $product['weight'];
+        $this->products[] = $product;
     }
 }
