12fadd48ccc685a504c8c966a8fefee176fe3478
grandeljay <github@grandels.email>
2024-05-30 14:18:39 +0200

    chore(module): update to v0.4.2

--- src/includes/modules/shipping/grandeljayfreight.php
+++ src/includes/modules/shipping/grandeljayfreight.php
@@ -23,7 +23,7 @@ class grandeljayfreight extends StdModule
     private array $countries;
 
     public const NAME    = 'MODULE_SHIPPING_GRANDELJAYFREIGHT';
-    public const VERSION = '0.4.1';
+    public const VERSION = '0.4.2';
 
     /**
      * Used by modified to determine the cheapest shipping method. Should
